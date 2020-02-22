168d8398ad58c1f4a5ad5d6b821486f1ea5c2c00
Evgenia Shlonska <evgeniashlonska@inbox.lv>
2020-02-22 12:12:35 +0200

    Lab Activity 5

--- public/game.js
+++ public/game.js
@@ -1,7 +1,6 @@
 
 // Change "test" to any username you'd like to start a new game
-var username = "test";
-
+var username = "evgenia";
 var socket = io.connect("https://tictactoe.info/", {transports: ['websocket']});
 setupSocket();
 
@@ -10,32 +9,48 @@ function setupSocket() {
 
     // This function is called whenever a new game state is received from the server
     socket.on('gameState', function (jsonGameState) {
-        console.log(jsonGameState);
+        console.log(jsonGameState);        
+        const gameData = JSON.parse(jsonGameState);
+        const username = document.getElementById("username");
+        const gold = document.getElementById("gold");
+        const shovelName = document.getElementById("shovelName");
+        const shovelOwned = document.getElementById("shovelOwned");
+        const shovelCost = document.getElementById("shovelCost");
+        const excavatorName = document.getElementById("excavatorName");
+        const excavatorOwned = document.getElementById("excavatorOwned");
+        const excavatorCost = document.getElementById("excavatorCost");
+        const mineName = document.getElementById("mineName");
+        const mineOwned = document.getElementById("mineOwned");
+        const mineCost = document.getElementById("mineCost");
 
-        // TODO: Display the game state on your GUI
-        // You must display: current gold, and the name, number owned, and cost for each type of equipment
-
+        username.innerHTML = gameData.username;
+        gold.innerHTML = gameData.gold
+        shovelName.innerHTML = gameData.equipment.shovel.name
+        shovelOwned.innerHTML = gameData.equipment.shovel.numberOwned
+        shovelCost.innerHTML = gameData.equipment.shovel.cost
+        excavatorName.innerHTML = gameData.equipment.excavator.name
+        excavatorOwned.innerHTML = gameData.equipment.excavator.numberOwned
+        excavatorCost.innerHTML = gameData.equipment.excavator.cost
+        mineName.innerHTML = gameData.equipment.mine.name
+        mineOwned.innerHTML = gameData.equipment.mine.numberOwned
+        mineCost.innerHTML = gameData.equipment.mine.cost
     });
+        // TODO: Display the game state on your GUI
+        // You must display: current gold, and the name, number owned, and cost for each type of equipment   
 }
-
-
 function initializeGame() {
     socket.emit("register", username);
 
     // TODO: Add any initialization code you'd like to setup your GUI
     // This function is called once when the page loads
-
 }
-
-
 // Call this function whenever the user clicks your gold button
 function clickGold() {
     socket.emit("clickGold");
 }
-
-
 // Call this function whenever the user clicks to purchase equipment
 // The parameter is the id of the equipment type to purchase
 function buyEquipment(equipmentID) {
     socket.emit("buy", equipmentID);
 }
+
